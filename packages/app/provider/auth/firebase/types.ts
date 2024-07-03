38ee0745e360075688ff3bc27999acdcaa77741e
rizamoyi <samwelmoyi@gmail.com>
2024-07-04 01:23:57 +0300

    feat: add signInwith phoneNumber auth functionality

--- packages/app/provider/auth/firebase/types.ts
+++ packages/app/provider/auth/firebase/types.ts
@@ -8,4 +8,5 @@ export type Firebase = {
   signOut: () => Promise<void>
   onAuthStateChanged: (callback: (user: { uid: string } | null) => void) => () => void
   getCurrentUser: () => User | null
+  // signInWithPhoneNumber: (phoneNumber: string) => Promise<firebase.ConfirmationResult>
 }
