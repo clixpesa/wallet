35b3a7e6738fed594998c7e57ab7bfc659820d16
rizamoyi <samwelmoyi@gmail.com>
2024-08-01 00:51:52 +0300

    feat: add nuggets and space card components

---
+++ packages/ui/src/components/ActionButton.tsx
@@ -0,0 +1,19 @@
+import { IconProps } from '@tamagui/helpers-icon'
+import { Button } from 'tamagui'
+
+export type ActionButtonProps = {
+  buttonText?: string
+  action: () => void
+  icon: React.FC<IconProps>
+}
+
+export const ActionButton = ({ buttonText, action, icon: Icon }: ActionButtonProps) => {
+  return (
+    <Button size="$4" onPress={action} theme="green">
+      <Button.Icon>
+        <Icon />
+      </Button.Icon>
+      {buttonText && <Button.Text>{buttonText}</Button.Text>}
+    </Button>
+  )
+}
---
+++ packages/ui/src/components/SpaceCard.tsx
@@ -0,0 +1,86 @@
+import { LinearGradient } from '@tamagui/linear-gradient'
+import {
+  Avatar,
+  Card,
+  CardProps,
+  H4,
+  Image,
+  Paragraph,
+  TooltipSimple,
+  XStack,
+  YStack,
+} from 'tamagui'
+
+export const SpaceCard = ({
+  title,
+  description,
+  authors,
+  withImages,
+  ...props
+}: {
+  title: string
+  description?: string
+  authors?: { avatar: string; name: string; id: number }[]
+  withImages?: boolean
+} & CardProps) => {
+  return (
+    <Card br="$4" overflow="hidden" {...props}>
+      <Card.Header>
+        {withImages && (
+          <Image
+            br="$4"
+            source={{
+              uri: `https://picsum.photos/seed/${title}/1000/500`,
+            }}
+            h={150}
+            $sm={{
+              h: 150,
+            }}
+          />
+        )}
+
+        <XStack jc="space-between" ai="center">
+          <YStack pt="$4" gap="$2">
+            <H4 size="$5" tt="capitalize">
+              {title}
+            </H4>
+            {!!description && (
+              <Paragraph size="$3" theme="alt1">
+                {description}
+              </Paragraph>
+            )}
+          </YStack>
+
+          {authors && authors.length > 0 && (
+            <XStack>
+              {authors.map((author) => (
+                <TooltipSimple key={author.id} label={author.name}>
+                  <Avatar circular size={34} mr="$-2">
+                    <Avatar.Image
+                      source={{
+                        uri: author.avatar,
+                        width: 34,
+                        height: 34,
+                      }}
+                    />
+                  </Avatar>
+                </TooltipSimple>
+              ))}
+            </XStack>
+          )}
+        </XStack>
+      </Card.Header>
+
+      <Card.Background>
+        <LinearGradient
+          // br="$4"
+          w="100%"
+          h="100%"
+          colors={['$color2', '$color1']}
+          start={[1, 1]}
+          end={[0.85, 0]}
+        />
+      </Card.Background>
+    </Card>
+  )
+}
