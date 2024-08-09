1c06668762c122e89b71766bfefd011816e3ebfa
rizamoyi <samwelmoyi@gmail.com>
2024-08-10 00:40:48 +0300

    chore(ui): use clixpesa theme colors

--- packages/ui/src/components/ActionButton.tsx
+++ packages/ui/src/components/ActionButton.tsx
@@ -9,11 +9,11 @@ export type ActionButtonProps = {
 
 export const ActionButton = ({ buttonText, action, icon: Icon }: ActionButtonProps) => {
   return (
-    <Button size="$4" onPress={action} theme="teal">
-      <Button.Icon>
+    <Button  onPress={action} theme="teal" >
+      <Button.Icon >
         <Icon />
       </Button.Icon>
-      {buttonText && <Button.Text >{buttonText}</Button.Text>}
+      {buttonText && <Button.Text   >{buttonText}</Button.Text>}
     </Button>
   )
 }
