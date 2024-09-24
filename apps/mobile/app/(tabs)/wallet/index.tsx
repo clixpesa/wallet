0fb124bd3b16c580de09c0bb8576d09c2f067bf4
rizamoyi <samwelmoyi@gmail.com>
2024-09-24 12:40:59 +0300

    feat: add chip compoents

--- apps/mobile/app/(tabs)/wallet/index.tsx
+++ apps/mobile/app/(tabs)/wallet/index.tsx
@@ -1,10 +1,10 @@
 import { SafeAreaView } from 'react-native-safe-area-context'
-import { Text } from 'ui'
+import { Text, ChipsWithIcon } from 'ui'
 
 export default function WalletScreen() {
   return (
     <SafeAreaView style={{ flex: 1 }} edges={['left', 'right']}>
-      <Text>Wallet Screen</Text>
+      <ChipsWithIcon />
     </SafeAreaView>
   )
 }
