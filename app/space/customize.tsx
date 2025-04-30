57f8730c071f8766bfbe5c76d8a16f545ade93c0
rizamoyi <samwelmoyi@gmail.com>
2025-04-30 18:41:15 +0300

    customize space

--- app/space/customize.tsx
+++ app/space/customize.tsx
@@ -1,187 +1,113 @@
-import { useHeaderHeight } from '@react-navigation/elements'
-import { useLocalSearchParams, useNavigation } from 'expo-router'
-import { View, useTheme, H2 } from 'tamagui'
-import { Platform, useColorScheme } from 'react-native'
-import { useSafeAreaInsets } from 'react-native-safe-area-context'
-import { ScrollView } from 'react-native-gesture-handler'
-import { Input } from 'components/forms/inputs/components/inputParts'
+import { FormProvider, useForm, useWatch } from 'react-hook-form'
+import { View, useTheme, H2, Theme, Text } from 'tamagui'
+import { z } from 'zod'
 
-import Animated, {
-  useSharedValue,
-  useAnimatedStyle,
-  useAnimatedScrollHandler,
-  interpolate,
-  Extrapolation,
-  type SharedValue,
-} from 'react-native-reanimated'
+import Animated, { useSharedValue, useAnimatedScrollHandler } from 'react-native-reanimated'
 
 import { SubmitButton, Chip } from 'components'
-
-const AnimatedScrollView = Animated.createAnimatedComponent(ScrollView)
+import { SchemaForm, formFields } from 'utils/SchemaForm'
 
 const spaceNames = ['Holidays', 'Savings', 'Gift', 'Rainy day', 'Education', 'Renovation']
 
+const SpaceSchema = z.object({
+  space: formFields.text.min(1).max(30),
+})
+
 export default function Customize() {
   const theme = useTheme()
-  const navigation = useNavigation()
-  const params = useLocalSearchParams()
+  const form = useForm()
 
-  // Animated header on scroll
-  const translationY = useSharedValue(0)
-  const scrollHandler = useAnimatedScrollHandler((event) => {
-    translationY.value = event.contentOffset.y
+  // Watch the 'space' field value
+  const spaceValue = useWatch({
+    control: form.control,
+    name: 'space',
   })
 
-  const insets = useSafeAreaInsets()
+  console.log('Space Value', spaceValue)
+
+  // Disable button if field is empty or invalid
+  const isDisabled = !spaceValue?.trim()
+
+  const handleSpaceCreation = ({ space }) => {
+    alert('Space created!!')
+    console.log('space', space)
+  }
 
   return (
     <View style={{ flex: 1 }}>
-      <>
-        <AnimatedScrollView
-          style={{ flex: 1 }}
-          onScroll={scrollHandler}
-          scrollEventThrottle={8}
-          contentContainerStyle={[
-            {
-              borderBottomRightRadius: 20,
-              borderBottomLeftRadius: 20,
-            },
-            {
-              paddingBottom: insets.bottom + 24,
-            },
-          ]}
-        >
-          <Animated.View
-            style={[
-              {
-                height: 250,
-                paddingTop: 50,
-                paddingHorizontal: 16,
-                overflow: 'hidden',
-                backgroundColor: theme.teal8.val,
-              },
-            ]}
-          >
-            {/* <Image
+      <Animated.View
+        style={[
+          {
+            height: 250,
+            paddingTop: 50,
+            paddingHorizontal: 16,
+            marginBottom: 16,
+            overflow: 'hidden',
+            backgroundColor: theme.teal8.val,
+          },
+        ]}
+      >
+        {/* <Image
                 tintColor={iconColor}
                 source={require('../../assets/images/react-logo.png')}
                 style={styles.reactLogo}
               /> */}
-            <View
-              style={{
-                flex: 1,
-                // backgroundColor: 'green',
-                // justifyContent: 'center',
-              }}
-            >
-              <H2 fontFamily="$heading" size="$2" my="$7">
-                Customize
-              </H2>
-              {/* <Text fontWeight="bold" fontSize={32} style={styles.talkTitle}>
-                {talk?.title}
-              </Text> */}
-            </View>
-          </Animated.View>
-          <View
-            style={{
-              paddingTop: 16,
-              paddingHorizontal: 16,
-            }}
-          >
-            <View flexDirection="row" flexWrap="wrap" gap="$3">
-              {spaceNames.map((name) => (
-                <Chip
-                  theme="teal_alt1"
-                  bg="$teal4"
-                  circular
-                  key={name}
-                  pressable
-                  size="$2.5"
-                  onPress={() => {
-                    setTimeout(() => {
-                      alert('pressed')
-                    })
-                  }}
-                >
-                  <Chip.Text>{name}</Chip.Text>
-                </Chip>
-              ))}
-            </View>
-          </View>
-        </AnimatedScrollView>
+        <View
+          style={{
+            flex: 1,
+          }}
+        >
+          <H2 fontFamily="$heading" size="$2" my="$7">
+            Customize
+          </H2>
+        </View>
+      </Animated.View>
+      <FormProvider {...form}>
+        <SchemaForm
+          onSubmit={handleSpaceCreation}
+          schema={SpaceSchema}
+          props={{
+            space: {
+              placeholder: 'Name your Space',
+            },
+          }}
+          renderAfter={({ submit }) => (
+            <Theme inverse>
+              <SubmitButton
+                onPress={() => submit()}
+                rounded="$10"
+                theme="teal"
+                disabled={isDisabled}
+                // opacity={isDisabled ? 0.5 : 1}
+              >
+                Create my space
+              </SubmitButton>
+            </Theme>
+          )}
+        >
+          {(fields) => (
+            <>
+              {Object.values(fields)}
 
-        {Platform.OS === 'android' ? (
-          <HeaderBackgroundAndroid scrollTranslationY={translationY} />
-        ) : (
-          <HeaderBackgroundIOS scrollTranslationY={translationY} />
-        )}
-      </>
+              <View flexDirection="row" flexWrap="wrap" gap="$2" mt="$4">
+                {spaceNames.map((name) => (
+                  <Chip
+                    theme="teal_alt1"
+                    bg="$teal4"
+                    circular
+                    key={name}
+                    pressable
+                    size="$2.5"
+                    onPress={() => form.setValue('space', name)}
+                  >
+                    <Chip.Text>{name}</Chip.Text>
+                  </Chip>
+                ))}
+              </View>
+            </>
+          )}
+        </SchemaForm>
+      </FormProvider>
     </View>
-  )
-}
-
-// We use a transparent header background on Android to provide a nice looking
-// header that expands to the top of the screen. This component ensures that
-// a header background becomes visible as we scroll past the header, so we don't
-// just see a floating back button.
-function HeaderBackgroundAndroid({
-  scrollTranslationY,
-}: {
-  scrollTranslationY: SharedValue<number>
-}) {
-  const theme = useTheme()
-  const headerHeight = useHeaderHeight()
-  const animatedStyle = useAnimatedStyle(() => ({
-    opacity: interpolate(scrollTranslationY.value, [50, 150], [0, 1]),
-  }))
-
-  return (
-    <Animated.View
-      style={[
-        animatedStyle,
-        {
-          height: headerHeight,
-          position: 'absolute',
-          elevation: 4,
-          top: 0,
-          left: 0,
-          right: 0,
-          backgroundColor: theme.teal10.val,
-        },
-      ]}
-    />
-  )
-}
-
-function HeaderBackgroundIOS({
-  scrollTranslationY,
-}: {
-  scrollTranslationY: SharedValue<number>
-}) {
-  const headerHeight = useHeaderHeight()
-  const colorScheme = useColorScheme()
-  const animatedStyle = useAnimatedStyle(() => ({
-    opacity: interpolate(scrollTranslationY.value, [0, 150], [0, 1], Extrapolation.CLAMP),
-  }))
-
-  return (
-    <Animated.View
-      style={[
-        animatedStyle,
-        {
-          position: 'absolute',
-          elevation: 4,
-          top: 0,
-          left: 0,
-          right: 0,
-        },
-      ]}
-    >
-      {/* <BlurView
-        intensity={40}
-        tint={colorScheme === 'light' ? 'systemThinMaterialLight' : 'systemThinMaterialDark'}
-        style={{ height: headerHeight, flex: 1 }}
-      /> */}
-    </Animated.View>
   )
 }
